a758e61b232f2af14191bac5c1073c127529670c
arnavsharma0809 <108084843+arnavsharma0809@users.noreply.github.com>
2022-07-14 08:54:32 +0530

    Add files via upload

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Navbar(){
+  return(
+
+   <div class="set">
+     <button class="w drum">w</button>
+     <button class="a drum">a</button>
+     <button class="s drum">s</button>
+     <button class="d drum">d</button>
+     <button class="j drum">j</button>
+     <button class="k drum">k</button>
+     <button class="l drum">l</button>
+   </div>
+
+
+  );
+
+}
+export default Navbar;
